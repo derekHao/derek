fe79abcddd4e32b1c453d3adfc5aee4caac7a35a
derek.hao <derek89757@gmail.com>
2018-02-03 18:26:09 +0800

    20180203

--- js/task_2_1_1.js
+++ js/task_2_1_1.js
@@ -2,8 +2,8 @@ var myHeading = document.querySelector("p");//网页第一个p元素
 myHeading.innerHTML = 'hello world';
 var btn = document.getElementById('button');
 ipt = document.getElementById("aqi-input");
-var name = prompt('输入姓名');
-alert(name)
+
+
 
 btn.onclick = function(){
     document.querySelector('span').innerHTML = document.getElementById("aqi-input").value;
---
+++ js/task_2_1_2.js
@@ -0,0 +1,24 @@
+ var aqiData = [
+    ["北京", 90],
+    ["上海", 50],
+    ["福州", 10],
+    ["广州", 50],
+    ["成都", 90],
+    ["西安", 100]
+];
+}
+(function ad(){
+    var newAqiData =[];
+    for(i=0;i<aqiData.length;i++){
+        if (aqiData[i][1] > 60)
+        newAqiData.push(aqiData[i])
+    }
+    for(i=0;i<newAqiData.length;i++){
+        if newAqiData[i][1]
+    }
+
+})();
+
+
+
+
