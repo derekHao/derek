a6f546c7ba7cc48cad9233a2fa69c7d01c984ee5
derek.hao <derek89757@gmail.com>
2018-02-05 22:21:33 +0800

    20180205

--- js/task_2_1_3.js
+++ js/task_2_1_3.js
@@ -4,46 +4,46 @@
  * 返回一个数组，格式见函数中示例
  */
 var ul_s = document.getElementById('source');
-alert(substr(ul_s.children[0].innerHTML,0,1))
- 
+var N = [  "零", "一", "二", "三", "四", "五", "六", "七", "八", "九","十"];
+var ul_r = document.getElementById('resort');
+var btn = document.getElementById('sort-btn');
+// alert(ul_s.children[0].innerHTML.substr(0,2))
 function getData() {
-    /*
-    coding here
-    */
-  
-    /*
-    data = [
-      ["北京", 90],
-      ["北京", 90]
-      ……
-    ]
-    */
-    var date =[];
-    var ul_s = document.getElementById('scurce');
-    date.foreach(function(item,index,array){
-        ul_s.children[index].innerHTML
-
-    });
-    return data;
-  
+    var ul_s = document.getElementById('source');
+    var index = ul_s.children.length;
+    var data = new Array(index);
+    for(i=0;i<index;i++){
+      var node =[];
+      node[0]=ul_s.children[i].innerHTML.substr(0,2);
+      node[1]=ul_s.children[i].children[0].innerHTML;
+      data[i] = node;
+    }
+     return data;
   }
-  
   /**
    * sortAqiData
    * 按空气质量对data进行从小到大的排序
    * 返回一个排序后的数组
    */
   function sortAqiData(data) {
-  
+    data.sort(
+      function(a,b){
+        return a[1]-b[1];
+      }
+    )
+    return data;
   }
-  
   /**
    * render
    * 将排好序的城市及空气质量指数，输出显示到id位resort的列表中
    * 格式见ul中的注释的部分
    */
-  function render(data) {
-  
+  function render(data) {   
+    data.forEach(function(item,index,array){
+      var node=document.createElement('li');
+      node.innerHTML = "第"+N[index+1]+"名："+item[0]+"空气质量："+item[1];
+      ul_r.appendChild(node)
+    })
   }
   
   function btnHandle() {
@@ -53,9 +53,9 @@ function getData() {
   }
   
   function init() {
-  
     // 在这下面给sort-btn绑定一个点击事件，点击时触发btnHandle函数
-  
-  }
+      btn.onclick = function(){
+      btnHandle();
+  }}
   
   init();
